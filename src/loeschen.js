4b976e1de9713830f641637220a52d9df54988d1
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-03-20 16:57:20 +0100

    dazu: npm run loeschen

---
+++ src/loeschen.js
@@ -0,0 +1,31 @@
+import mqtt from "mqtt";
+import os   from "os";
+
+import { mqttKonfiguration } from "./mqttKonfigurationen.js";
+
+
+const authObjekt = {
+
+    username: mqttKonfiguration.nutzername,
+    password: mqttKonfiguration.passwort,
+};
+
+const mqttClient =
+        await mqtt.connectAsync( mqttKonfiguration.serverUrl,
+                                 authObjekt );
+
+const hostname = os.hostname();
+const topic    = mqttKonfiguration.basisTopic + hostname;
+
+const konfigObjekt = {
+                        qos: 2,
+                        retain: true
+                     };
+
+await mqttClient.publishAsync( topic, "", konfigObjekt );
+
+console.log( `Metrik gelöscht auf Topic ${topic}.` );
+
+await mqttClient.endAsync();
+
+console.log();
